d59a5efc492e8c8aed67739e76ee67d65fa0c27a
Park Ju Hyung <qkrwngud825@gmail.com>
2016-06-19 22:42:48 +0900

    releasetools: don't build recovery for soundcontrol
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- build_master.sh
+++ build_master.sh
@@ -2,7 +2,6 @@
 if [ ! "${1}" = "skip" ] ; then
 	./build_clean.sh
 	./build_kernel.sh CC='$(CROSS_COMPILE)gcc' "$@" || exit 1
-	./build_recovery.sh CC='$(CROSS_COMPILE)gcc' "$@" || exit 1
 fi
 
 if [ -e boot.img ] ; then
@@ -16,19 +15,4 @@ if [ -e boot.img ] ; then
 	cd ..
 	ls -al arter97-kernel-"$(git rev-parse --abbrev-ref HEAD)"-"$(cat version)".zip
 	rm kernelzip/boot.img kernelzip/kernel.prop
-fi
-
-if [ -e recovery.img ] ; then
-	rm arter97-recovery-"$(cat version)"-twrp_"$(cat version_recovery | awk '{print $1}')".zip 2>/dev/null
-	cp recovery.img recoveryzip/
-	cd recoveryzip/
-	7z a -mx9 arter97-recovery-"$(git rev-parse --abbrev-ref HEAD)"-"$(cat ../version)"-twrp_"$(cat ../version_recovery | awk '{print $1}')"-tmp.zip *
-	zipalign -v 4 arter97-recovery-"$(git rev-parse --abbrev-ref HEAD)"-"$(cat ../version)"-twrp_"$(cat ../version_recovery | awk '{print $1}')"-tmp.zip ../arter97-recovery-"$(git rev-parse --abbrev-ref HEAD)"-"$(cat ../version)"-twrp_"$(cat ../version_recovery | awk '{print $1}')".zip
-	rm arter97-recovery-"$(git rev-parse --abbrev-ref HEAD)"-"$(cat ../version)"-twrp_"$(cat ../version_recovery | awk '{print $1}')"-tmp.zip
-	cd ..
-	ls -al arter97-recovery-"$(git rev-parse --abbrev-ref HEAD)"-"$(cat version)"-twrp_"$(cat version_recovery | awk '{print $1}')".zip
-	rm recoveryzip/recovery.img
-	fakeroot tar -H ustar -c recovery.img > arter97-recovery-"$(git rev-parse --abbrev-ref HEAD)"-"$(cat version)"-twrp_"$(cat version_recovery | awk '{print $1}')".tar
-	md5sum -t arter97-recovery-"$(git rev-parse --abbrev-ref HEAD)"-"$(cat version)"-twrp_"$(cat version_recovery | awk '{print $1}')".tar >> arter97-recovery-"$(git rev-parse --abbrev-ref HEAD)"-"$(cat version)"-twrp_"$(cat version_recovery | awk '{print $1}')".tar
-	mv arter97-recovery-"$(git rev-parse --abbrev-ref HEAD)"-"$(cat version)"-twrp_"$(cat version_recovery | awk '{print $1}')".tar arter97-recovery-"$(git rev-parse --abbrev-ref HEAD)"-"$(cat version)"-twrp_"$(cat version_recovery | awk '{print $1}')".tar.md5
 fi
